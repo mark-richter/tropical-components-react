1000a76f06327e68f290beea60c1b9f4b84a7b12
Josh Brown <joshb@hbttrenchless.com>
2018-06-07 13:20:49 -0600

    fixed dynamic form export

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import DynamicFormWinterfell from "./Forms/DynamicFormWinterfell";
-
 // Cards
 export Card from "./Card/Card.jsx";
 export CardImage from "./Card/CardImage.jsx";
+
+// Forms
 export DynamicFormWinterfell from "./Forms/DynamicFormWinterfell.jsx";
