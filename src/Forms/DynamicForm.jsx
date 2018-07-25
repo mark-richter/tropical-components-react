eeea06c0a7086e9756f60ad58ecffcb621c190b6
Fr0nk3ns33n <josh@masterminddesign.io>
2018-07-25 14:50:48 -0600

    Added Creatable Select - Using React-Select third party library - Test inside Dynamic Form Component

--- src/Forms/DynamicForm.jsx
+++ src/Forms/DynamicForm.jsx
@@ -1,63 +1,66 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 var Winterfell = require("winterfell");
+import CreatableSingleSelect from "./CreatableSingleSelect.jsx";
+
+Winterfell.addInputType("creatableSingleSelect", CreatableSingleSelect);
 
 class DynamicForm extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            data: this.props.data
-        };
-        this.onRender = this.onRender.bind(this);
-        this.onUpdate = this.onUpdate.bind(this);
-        this.onSubmit = this.onSubmit.bind(this);
-    }
-
-    static propTypes = {
-        schema: PropTypes.object.isRequired,
-        data: PropTypes.object,
-        onSubmit: PropTypes.func.isRequired
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: this.props.data
     };
+    this.onRender = this.onRender.bind(this);
+    this.onUpdate = this.onUpdate.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
 
-    static defaultProps = {
-        data: {}
-    };
+  static propTypes = {
+    schema: PropTypes.object.isRequired,
+    data: PropTypes.object,
+    onSubmit: PropTypes.func.isRequired
+  };
 
-    componentWillReceiveProps(nextProps) {
-        this.setState({ questionAnswers: nextProps });
-    }
+  static defaultProps = {
+    data: {}
+  };
 
-    onRender() {
-        //console.log("Great news! Winterfell rendered successfully");
-        //this.setState({ "[questionAnswers]": this.props.data });
-    }
+  componentWillReceiveProps(nextProps) {
+    this.setState({ questionAnswers: nextProps });
+  }
 
-    onUpdate(questionAnswers) {
-        // console.log(
-        //     "Question Updated! The current set of answers is: ",
-        //     this.state.data
-        // );
-        this.setState({ data: questionAnswers });
-    }
+  onRender() {
+    //console.log("Great news! Winterfell rendered successfully");
+    //this.setState({ "[questionAnswers]": this.props.data });
+  }
 
-    onSubmit(questionAnswers, target) {
-        this.props.onSubmit(questionAnswers);
-    }
+  onUpdate(questionAnswers) {
+    // console.log(
+    //     "Question Updated! The current set of answers is: ",
+    //     this.state.data
+    // );
+    this.setState({ data: questionAnswers });
+  }
 
-    render() {
-        return (
-            <div className="row">
-                <Winterfell
-                    schema={this.props.schema}
-                    disableSubmit={true}
-                    onRender={this.onRender}
-                    onUpdate={this.onUpdate}
-                    onSubmit={this.onSubmit}
-                    questionAnswers={this.state.data}
-                />
-            </div>
-        );
-    }
+  onSubmit(questionAnswers, target) {
+    this.props.onSubmit(questionAnswers);
+  }
+
+  render() {
+    return (
+      <div className="row">
+        <Winterfell
+          schema={this.props.schema}
+          disableSubmit={true}
+          onRender={this.onRender}
+          onUpdate={this.onUpdate}
+          onSubmit={this.onSubmit}
+          questionAnswers={this.state.data}
+        />
+      </div>
+    );
+  }
 }
 
 export default DynamicForm;
